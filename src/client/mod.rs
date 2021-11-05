24c7f0d14689199b0d43d794b07db67785a15e97
Walfie <walfington@gmail.com>
2021-11-04 21:39:09 -0400

    Implement Service for Client

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,9 +3,12 @@ use crate::error::{Error, MultiplexError};
 
 use futures_core::TryStream;
 use futures_sink::Sink;
+use futures_util::TryFutureExt;
 use std::convert::TryFrom;
 use std::error::Error as StdError;
+use std::future::Future;
 use std::pin::Pin;
+use std::task::{Context, Poll};
 use tokio_tower::multiplex::{Client as MultiplexClient, MultiplexTransport, TagStore};
 use tower::util::ServiceExt;
 use tower::Service;
@@ -72,5 +75,64 @@ where
                 received: e,
             }),
         }
+    }
+}
+
+impl<T> Service<RequestEnvelope> for Client<T>
+where
+    T: Sink<RequestEnvelope> + TryStream<Ok = ResponseEnvelope> + Send + 'static,
+    <T as Sink<RequestEnvelope>>::Error: StdError + Send,
+    <T as TryStream>::Error: StdError + Send,
+{
+    type Response = ResponseEnvelope;
+    type Error = Error<<T as TryStream>::Error, <T as Sink<RequestEnvelope>>::Error>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.client.poll_ready(cx).map_err(Error::from)
+    }
+
+    fn call(&mut self, req: RequestEnvelope) -> Self::Future {
+        Box::pin(self.client.call(req).map_err(Error::from))
+    }
+}
+
+impl<T, Req> Service<Req> for Client<T>
+where
+    T: Sink<RequestEnvelope> + TryStream<Ok = ResponseEnvelope> + Send + 'static,
+    Req: Request + Send,
+    <T as Sink<RequestEnvelope>>::Error: StdError + Send,
+    <T as TryStream>::Error: StdError + Send,
+{
+    type Response = Req::Response;
+
+    type Error = Error<<T as TryStream>::Error, <T as Sink<RequestEnvelope>>::Error>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.client.poll_ready(cx).map_err(Error::from)
+    }
+
+    fn call(&mut self, req: Req) -> Self::Future {
+        let msg = RequestEnvelope::new(req.into());
+
+        use futures_util::future::ready;
+
+        let resp = self
+            .client
+            .call(msg)
+            .map_err(Self::Error::from)
+            .and_then(|resp| {
+                ready(match Req::Response::try_from(resp.data) {
+                    Ok(data) => Ok(data),
+                    Err(ResponseData::ApiError(e)) => Err(Error::Api(e)),
+                    Err(e) => Err(Error::UnexpectedResponse {
+                        expected: Req::Response::MESSAGE_TYPE,
+                        received: e,
+                    }),
+                })
+            });
+
+        Box::pin(resp)
     }
 }
