e564ecd51a1361ace4a8ca1c91dbe929c6ae6c45
Walfie <walfington@gmail.com>
2022-10-17 23:07:30 -0400

    Add EventTransport that removes events

--- src/transport/event.rs
+++ src/transport/event.rs
@@ -1,36 +1,89 @@
 use crate::data::{EventData, RequestEnvelope, ResponseEnvelope};
-use crate::error::BoxError;
+use crate::error::{BoxError, Error};
 
 use futures_core::{Stream, TryStream};
 use futures_sink::Sink;
+use futures_util::stream::{SplitSink, SplitStream};
+use futures_util::StreamExt;
 use pin_project_lite::pin_project;
+use split_stream_by::{Either, LeftSplitByMap, RightSplitByMap, SplitStreamByMapExt};
+use std::fmt;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 pin_project! {
     /// A transport that forwards events to a separate sink.
-    #[derive(Debug, Clone)]
-    pub(crate) struct EventTransport<T> {
+    pub struct EventTransport<T, E> {
         #[pin]
-        transport: T,
+        sink: SplitSink<T, RequestEnvelope>,
+        #[pin]
+        stream: RightSplitByMap<
+            Result<ResponseEnvelope, E>,
+            Result<EventData, Error>,
+            Result<ResponseEnvelope, E>,
+            SplitStream<T>,
+            SplitFn<E>,
+        >,
     }
 }
 
-impl<T> EventTransport<T>
+pin_project! {
+    /// A stream of [`Event`]s.
+    pub struct EventStream<T, E> {
+        #[pin]
+        events: LeftSplitByMap<
+            Result<ResponseEnvelope, E>,
+            Result<EventData, Error>,
+            Result<ResponseEnvelope, E>,
+            SplitStream<T>,
+            SplitFn<E>,
+        >,
+    }
+}
+
+impl<T, E> fmt::Debug for EventTransport<T, E>
 where
-    T: Sink<RequestEnvelope> + TryStream,
+    T: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EventTransport")
+            .field("sink", &self.sink)
+            .field("stream", &"Stream") // RightSplitByMap doesn't impl Debug
+            .finish()
+    }
+}
+
+type SplitFn<E> = fn(
+    Result<ResponseEnvelope, E>,
+) -> Either<Result<EventData, Error>, Result<ResponseEnvelope, E>>;
+
+impl<T, E> EventTransport<T, E>
+where
+    T: Sink<RequestEnvelope> + Stream<Item = Result<ResponseEnvelope, E>> + Unpin + Send + 'static,
+    E: Send + 'static,
 {
     /// Creates a new [`ApiTransport`].
-    pub fn new<S>(transport: T, event_sink: S) -> Self
-    where
-        S: Sink<EventData>,
-    {
-        // TODO: Do something with event_sink
-        Self { transport }
+    pub fn new<S>(transport: T) -> (Self, EventStream<T, E>) {
+        let (resp_sink, resp_stream) = transport.split();
+
+        let (events, responses) = resp_stream.split_by_map(
+            (|resp| match resp {
+                Ok(r) if r.message_type().is_event() => Either::Left(r.parse_event()),
+                other => Either::Right(other),
+            }) as SplitFn<E>,
+        );
+
+        let event_stream = EventStream { events };
+        let transport_out = Self {
+            sink: resp_sink,
+            stream: responses,
+        };
+
+        (transport_out, event_stream)
     }
 }
 
-impl<T> Sink<RequestEnvelope> for EventTransport<T>
+impl<T, E> Sink<RequestEnvelope> for EventTransport<T, E>
 where
     T: Sink<RequestEnvelope>,
     BoxError: From<T::Error>,
@@ -40,7 +93,7 @@ where
     fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.as_mut()
             .project()
-            .transport
+            .sink
             .poll_ready(cx)
             .map_err(BoxError::from)
     }
@@ -48,7 +101,7 @@ where
     fn start_send(mut self: Pin<&mut Self>, item: RequestEnvelope) -> Result<(), Self::Error> {
         self.as_mut()
             .project()
-            .transport
+            .sink
             .start_send(item)
             .map_err(BoxError::from)
     }
@@ -56,7 +109,7 @@ where
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.as_mut()
             .project()
-            .transport
+            .sink
             .poll_flush(cx)
             .map_err(BoxError::from)
     }
@@ -64,23 +117,31 @@ where
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.as_mut()
             .project()
-            .transport
+            .sink
             .poll_close(cx)
             .map_err(BoxError::from)
     }
 }
 
-impl<T> Stream for EventTransport<T>
+impl<T, E> Stream for EventTransport<T, E>
 where
-    T: TryStream<Ok = ResponseEnvelope>,
-    T::Error: Into<BoxError>,
+    T: Stream<Item = Result<ResponseEnvelope, E>>,
+    E: Into<BoxError>,
 {
     type Item = Result<ResponseEnvelope, BoxError>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.project()
-            .transport
-            .try_poll_next(cx)
-            .map_err(Into::into)
+        self.project().stream.try_poll_next(cx).map_err(Into::into)
+    }
+}
+
+impl<T, E> Stream for EventStream<T, E>
+where
+    T: Stream<Item = Result<ResponseEnvelope, E>>,
+{
+    type Item = Result<EventData, Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.project().events.poll_next(cx)
     }
 }
--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod api;
 pub(crate) mod event;
 
 pub use crate::transport::api::ApiTransport;
+pub use crate::transport::event::{EventStream, EventTransport};
 
 crate::cfg_feature! {
     #![feature = "tokio-tungstenite"]
